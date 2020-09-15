95d0ef13eceae118ac0d3ca76bef44d6575d6652
lipengzhou <lpzmail@163.com>
2020-09-16 03:08:08 +0800

    导入element组件库

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     node: true
   },
+  // 使用插件的编码校验规则
   extends: [
     'plugin:vue/essential',
     '@vue/standard',
@@ -11,8 +12,17 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
+
+  // 自定义编码校验规则
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'semi': ['error', 'always']
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      "multiline": {
+        "delimiter": "none",
+        "requireLast": true
+      }
+    }]
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,12 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App'
+})
+</script>
+
 <style lang="scss" scoped></style>
