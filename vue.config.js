51a08bcf6a24008ae264392f15832e09cd4c73ee
lipengzhou <lpzmail@163.com>
2020-09-16 04:16:52 +0800

    接口处理-配置后端代理

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,18 @@ module.exports = {
         prependData: `@import "~@/styles/variables.scss";`
       }
     }
+  },
+
+  devServer: {
+    proxy: {
+      '/boss': {
+        target: 'http://eduboss.lagou.com',
+        changeOrigin: true // 把请求头中的 host 配置为 target
+      },
+      '/front': {
+        target: 'http://edufront.lagou.com',
+        changeOrigin: true
+      }
+    }
   }
 }
