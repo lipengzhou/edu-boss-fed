d60f755beda92a6686018dd3fa4c99df8d08e007
lipengzhou <lpzmail@163.com>
2020-09-23 20:09:37 +0800

    登录-表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="login">
+    <!--
+      1. :model="ruleForm"
+      2. :rules="rules"
+      3. ref="ruleForm"
+      4. el-form-item 绑定 prop 属性
+     -->
     <el-form
       class="login-form"
       label-position="top"
       ref="form"
       :model="form"
+      :rules="rules"
       label-width="80px"
     >
-      <el-form-item label="手机号">
+      <el-form-item label="手机号" prop="phone">
         <el-input v-model="form.phone"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
-        <el-input v-model="form.password"></el-input>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="form.password"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
@@ -24,6 +31,7 @@
 import Vue from 'vue'
 import request from '@/utils/request'
 import qs from 'qs'
+import { Form } from 'element-ui'
 
 export default Vue.extend({
   name: 'LoginIndex',
@@ -32,31 +40,47 @@ export default Vue.extend({
       form: {
         phone: '18201288771',
         password: '111111'
+      },
+      rules: {
+        phone: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { pattern: /^1\d{10}$/, message: '请输入正确的手机号', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+        ]
       }
     }
   },
 
   methods: {
     async onSubmit () {
-      // 1. 表单验证
-      // 2. 验证通过 -> 提交表单
-      const { data } = await request({
-        method: 'POST',
-        url: '/front/user/login',
-        headers: { 'content-type': 'application/x-www-form-urlencoded' },
-        data: qs.stringify(this.form) // axios 默认发送的是 application/json 格式的数据
-      })
+      try {
+        // 1. 表单验证
+        await (this.$refs.form as Form).validate()
 
-      // 3. 处理请求结果
-      //    失败：给出提示
-      if (data.state !== 1) {
-        return this.$message.error(data.message)
+        // 2. 验证通过 -> 提交表单
+        const { data } = await request({
+          method: 'POST',
+          url: '/front/user/login',
+          headers: { 'content-type': 'application/x-www-form-urlencoded' },
+          data: qs.stringify(this.form) // axios 默认发送的是 application/json 格式的数据
+        })
+
+        // 3. 处理请求结果
+        //    失败：给出提示
+        if (data.state !== 1) {
+          return this.$message.error(data.message)
+        }
+        //    成功：跳转到首页
+        this.$router.push({
+          name: 'home'
+        })
+        this.$message.success('登录成功')
+      } catch (err) {
+        console.log('登录失败', err)
       }
-      //    成功：跳转到首页
-      this.$router.push({
-        name: 'home'
-      })
-      this.$message.success('登录成功')
     }
   }
 })
