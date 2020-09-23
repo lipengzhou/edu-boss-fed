82010059ca92286290b55198a583d1e9154c395d
lipengzhou <lpzmail@163.com>
2020-09-23 16:52:04 +0800

    布局-侧边栏菜单

---
+++ src/layout/components/app-aside.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="aside">
+    <el-menu
+      default-active="4"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      router
+    >
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>权限管理</span>
+        </template>
+        <el-menu-item index="/role">
+          <i class="el-icon-setting"></i>
+          <span slot="title">角色管理</span>
+        </el-menu-item>
+        <el-menu-item index="/menu">
+          <i class="el-icon-setting"></i>
+          <span slot="title">菜单管理</span>
+        </el-menu-item>
+        <el-menu-item index="/resource">
+          <i class="el-icon-setting"></i>
+          <span slot="title">资源管理</span>
+        </el-menu-item>
+      </el-submenu>
+      <el-menu-item index="/course">
+        <i class="el-icon-menu"></i>
+        <span slot="title">课程管理</span>
+      </el-menu-item>
+      <el-menu-item index="/user">
+        <i class="el-icon-document"></i>
+        <span slot="title">用户管理</span>
+      </el-menu-item>
+      <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>广告管理</span>
+        </template>
+        <el-menu-item index="/advert">
+          <i class="el-icon-setting"></i>
+          <span slot="title">广告列表</span>
+        </el-menu-item>
+        <el-menu-item index="/advert-space">
+          <i class="el-icon-setting"></i>
+          <span slot="title">广告位列表</span>
+        </el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'AppAside',
+  methods: {
+    handleOpen (key: string, keyPath: string): void {
+      console.log(key, keyPath)
+    },
+
+    handleClose (key: string, keyPath: string): void {
+      console.log(key, keyPath)
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.aside {
+  .el-menu {
+    min-height: 100vh;
+  }
+}
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,18 +1,27 @@
 <template>
   <el-container>
-    <el-aside width="200px">Aside</el-aside>
+    <el-aside width="200px">
+      <app-aside />
+    </el-aside>
     <el-container>
       <el-header>Header</el-header>
-      <el-main>Main</el-main>
+      <el-main>
+        <!-- 子路由出口 -->
+        <router-view />
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import AppAside from './components/app-aside.vue'
 
 export default Vue.extend({
-  name: 'LayoutIndex'
+  name: 'LayoutIndex',
+  components: {
+    AppAside
+  }
 })
 </script>
 
@@ -21,6 +30,7 @@ export default Vue.extend({
   min-height: 100vh;
   min-width: 980px;
 }
+
 .el-aside {
   background: #d3dce6;
 }
