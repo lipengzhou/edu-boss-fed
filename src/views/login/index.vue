0020f5230e5b5c1f0211cc8161cdf0148a6834c3
lipengzhou <lpzmail@163.com>
2020-09-23 17:53:22 +0800

    登录-页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,67 @@
 <template>
-  <div class="login">用户登录</div>
+  <div class="login">
+    <el-form
+      class="login-form"
+      label-position="top"
+      ref="form"
+      :model="form"
+      label-width="80px"
+    >
+      <el-form-item label="手机号">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
+  },
+
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-form {
+    width: 300px;
+    background: #fff;
+    padding: 20px;
+    border-radius: 5px;
+  }
+  .login-btn {
+    width: 100%;
+  }
+}
+</style>
