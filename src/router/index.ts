a695389e69f9157fb0326599685873b1a03d8843
lipengzhou <lpzmail@163.com>
2020-09-25 15:59:16 +0800

    身份认证-校验页面访问权限

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Layout from '@/layout/index.vue'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -14,6 +15,9 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     component: Layout,
+    meta: {
+      requiresAuth: true
+    },
     children: [
       {
         path: '', // 默认子路由
@@ -66,6 +70,36 @@ const routes: Array<RouteConfig> = [
 
 const router = new VueRouter({
   routes
+})
+
+// 全局前置守卫：任何页面的访问都要经过这里
+// to：要去哪里的路由信息
+// from：从哪里来的路由信息
+// next：通行的标志
+router.beforeEach((to, from, next) => {
+  // console.log('进入了路由全局守卫')
+  console.log('to => ', to)
+  console.log('from => ', from)
+
+  // to.matched 是一个数组（匹配到是路由记录）
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.state.user) {
+      // 跳转到登录页面
+      next({
+        name: 'login'
+      })
+    } else {
+      next() // 允许通过
+    }
+  } else {
+    next() // 允许通过
+  }
+
+  // // 路由守卫中一定要调用 next，否则页码无法展示
+  // next()
+  // if (to.path !== '/login') {
+  //   // 校验登录状态
+  // }
 })
 
 export default router
