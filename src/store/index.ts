a37c4a74005697d1e1e2d0caee9e019111d5e531
lipengzhou <lpzmail@163.com>
2020-09-25 15:20:47 +0800

    身份认证-把登录状态存储到Vuex容器中

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,9 +4,20 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 容器的状态实现了数据共享，在组件里面访问方便，但是没有持久化的功能
   state: {
+    user: JSON.parse(window.localStorage.getItem('user') || 'null')
+    // user: null // 当前登录用户状态
   },
   mutations: {
+    // 修改容器数据必须使用 mutation 函数
+    setUser (state, payload) {
+      state.user = JSON.parse(payload)
+
+      // 为了防止页面刷新数据丢失，我们需要把 user 数据持久化
+      // 注意：本地存储只能存字符串
+      window.localStorage.setItem('user', payload)
+    }
   },
   actions: {
   },
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -82,6 +82,9 @@ export default Vue.extend({
         if (data.state !== 1) {
           this.$message.error(data.message)
         } else {
+          // 1. 登录成功，记录登录状态，状态需要能够全局访问（放到 Vuex 容器中）
+          this.$store.commit('setUser', data.content)
+          // 2. 然后在访问需要登录的页面的时候判断有没有登录状态（路由拦截器）
           //    成功：跳转到首页
           this.$router.push({
             name: 'home'
