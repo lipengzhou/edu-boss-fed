03c4902c41e7749b428298a3aaca3d6a9ad86b3f
lipengzhou <lpzmail@163.com>
2020-09-29 00:42:26 +0800

    处理Token过期-错误消息提示

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import store from '@/store'
+import { Message } from 'element-ui'
 
 const request = axios.create({
   // 配置选项
@@ -23,5 +24,35 @@ request.interceptors.request.use(function (config) {
 })
 
 // 响应拦截器
+request.interceptors.response.use(function (response) { // 状态码为 2xx 都会进入这里
+  // console.log('请求响应成功了 => ', response)
+  // 如果是自定义错误状态码，错误处理就写到这里
+  return response
+}, function (error) { // 超出 2xx 状态码都都执行这里
+  // console.log('请求响应失败了 => ', error)
+  // 如果是使用的 HTTP 状态码，错误处理就写到这里
+  // console.dir(error)
+  if (error.response) { // 请求发出去收到响应了，但是状态码超出了 2xx 范围
+    const { status } = error.response
+    if (status === 400) {
+      Message.error('请求参数错误')
+    } else if (status === 401) {
+      // token 无效（没有提供 token、token 是无效的、token 过期了）
+    } else if (status === 403) {
+      Message.error('没有权限，请联系管理员')
+    } else if (status === 404) {
+      Message.error('请求资源不存在')
+    } else if (status >= 500) {
+      Message.error('服务端错误，请联系管理员')
+    }
+  } else if (error.request) { // 请求发出去没有收到响应
+    Message.error('请求超时，请刷新重试')
+  } else { // 在设置请求时发生了一些事情，触发了一个错误
+    Message.error(`请求失败：${error.message}`)
+  }
+
+  // 把请求失败的错误对象继续抛出，扔给上一个调用者
+  return Promise.reject(error)
+})
 
 export default request
