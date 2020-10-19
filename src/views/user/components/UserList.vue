9497b92e4e53d4d02148abeafd4f1e50ca949ea5
lipengzhou <lpzmail@163.com>
2020-10-19 14:13:01 +0800

    用户管理-分配角色-准备

--- src/views/user/components/UserList.vue
+++ src/views/user/components/UserList.vue
@@ -81,10 +81,31 @@
         prop="address"
         label="操作">
         <template>
-          <el-button type="text">分配角色</el-button>
+          <el-button
+            type="text"
+            @click="dialogVisible = true"
+          >分配角色</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <el-dialog
+      title="分配角色"
+      :visible.sync="dialogVisible"
+      width="50%"
+    >
+      <el-select v-model="value1" multiple placeholder="请选择">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -106,7 +127,25 @@ export default Vue.extend({
         endCreateTime: '',
         rangeDate: []
       },
-      loading: true
+      loading: true,
+      dialogVisible: false,
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value1: []
     }
   },
 
