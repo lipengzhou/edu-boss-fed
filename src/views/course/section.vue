f7afe9283d9926cbb7f4358c98d82c3651d83517
lipengzhou <lpzmail@163.com>
2020-10-25 16:50:16 +0800

    课程内容-阶段列表-自定义树组件节点内容

--- src/views/course/section.vue
+++ src/views/course/section.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="course-section">
     <el-card>
-      <div slot="header">
+      <div class="card-header" slot="header">
         课程名称
+        <el-button type="primary">添加阶段</el-button>
       </div>
       <el-tree
         :data="sections"
         :props="defaultProps"
         draggable
-      ></el-tree>
+      >
+        <div class="inner" slot-scope="{ node, data }">
+          <span>{{ node.label }}</span>
+          <!-- section -->
+          <span v-if="data.sectionName" class="actions">
+            <el-button>编辑</el-button>
+            <el-button type="primary">添加课时</el-button>
+            <el-button>状态</el-button>
+          </span>
+          <!-- lession -->
+          <span v-else class="actions">
+            <el-button>编辑</el-button>
+            <el-button type="success">上传视频</el-button>
+            <el-button>状态</el-button>
+          </span>
+        </div>
+      </el-tree>
     </el-card>
   </div>
 </template>
@@ -50,4 +67,23 @@ export default Vue.extend({
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.inner {
+  flex: 1;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ebeef5;
+}
+
+::v-deep .el-tree-node__content {
+  height: auto;
+}
+</style>
