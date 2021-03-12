993369f7babedea2ac1816b13c0224648b35fdf6
lipengzhou <lpzmail@163.com>
2021-03-12 13:03:09 +0800

    refactor: do not build third-party packages

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css'
 
 // 加载全局样式
 import './styles/index.scss'
+
+if (process.env.NODE_ENV === 'development') {
+  require('element-ui/lib/theme-chalk/index.css')
+}
 
 Vue.use(ElementUI, {
   // size: 'small'
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
-// vue.config.js
-module.exports = {
+const isProd = process.env.NODE_ENV === 'production'
+
+const cdn = {
+  externals: {
+    'element-ui': 'ELEMENT',
+    vue: 'Vue',
+    moment: 'moment'
+  },
+  js: [
+    'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js',
+    'https://cdn.jsdelivr.net/npm/element-ui@2.14.1/lib/index.min.js',
+    'https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.min.js'
+  ],
+  css: [
+    'https://cdn.jsdelivr.net/npm/element-ui@2.14.1/lib/theme-chalk/index.css'
+  ]
+}
+
+const config = {
   css: {
     loaderOptions: {
       // 默认情况下 `sass` 选项会同时对 `sass` 和 `scss` 语法同时生效
@@ -10,14 +27,6 @@ module.exports = {
       scss: {
         prependData: '@import "~@/styles/variables.scss";'
       }
-    }
-  },
-
-  configureWebpack: {
-    externals: {
-      'element-ui': 'ELEMENT',
-      vue: 'Vue',
-      moment: 'moment'
     }
   },
 
@@ -39,5 +48,22 @@ module.exports = {
         }
       }
     }
-  }
+  },
+
+  chainWebpack: config => {
+    if (isProd) {
+      config.plugin('html').tap(args => {
+        args[0].cdn = cdn
+        return args
+      })
+    }
+  },
+
+  configureWebpack: {}
 }
+
+if (isProd) {
+  config.configureWebpack.externals = cdn.externals
+}
+
+module.exports = config
